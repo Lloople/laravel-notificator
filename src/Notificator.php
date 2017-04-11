37e02c34fa02b0bb52e8efc80678389274d3a4ac
vagrant <vagrant@homestead>
2017-04-11 19:00:52 +0000

    Initial commit

---
+++ src/Notificator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Notificator;
+
+/**
+ * Class Notificator
+ *
+ * @package Notificator
+ */
+class Notificator
+{
+
+    public static function success(string $message, int $duration = 5000)
+    {
+
+        self::createNotification('success', $message, $duration);
+    }
+
+    public static function createNotification(string $type, string $message, int $duration)
+    {
+
+        $notification = new NotificatorMessage($type, $message, $duration);
+
+        session()->flash('notifications.' . $notification->getId(), $notification);
+    }
+
+    public static function getUniqueId(): string
+    {
+
+        return uniqid();
+    }
+
+    public static function warning(string $message, int $duration = 5000)
+    {
+
+        self::createNotification('warning', $message, $duration);
+    }
+
+    public static function info(string $message, int $duration = 5000)
+    {
+
+        self::createNotification('info', $message, $duration);
+    }
+
+    public static function error(string $message, int $duration = 5000)
+    {
+
+        self::createNotification('error', $message, $duration);
+    }
+}
---
+++ src/NotificatorMessage.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Notificator;
+
+
+/**
+ * Class NotificatorMessage
+ *
+ * @package Notificator
+ */
+class NotificatorMessage
+{
+
+    private $type;
+    private $message;
+    private $duration;
+    private $id;
+    public function __construct(string $type, string $message, int $duration)
+    {
+        $this->id = uniqid();
+        $this->type = $type;
+        $this->message = $message;
+        $this->duration = $duration;
+    }
+
+    public function getType(): string { return $this->type; }
+
+    public function getMessage(): string { return $this->message; }
+
+    public function getDuration(): int { return $this->duration; }
+
+    public function getId(): string { return $this->id; }
+
+    public function getBootstrapClass()
+    {
+        return $this->type == 'error' ? 'danger' : $this->type;
+    }
+}
