1fdb52035cfb1b9d00db7cd6f42592f486dd6128
Lloople <d.lloople@gmail.com>
2017-04-28 19:33:04 +0200

    Delete unused method and add PHPDoc

--- src/Notificator.php
+++ src/Notificator.php
@@ -10,12 +10,21 @@ namespace Lloople\Notificator;
 class Notificator
 {
 
+    /**
+     * @param string $message
+     * @param int $duration
+     */
     public static function success(string $message, int $duration = 5000)
     {
 
         self::createNotification('success', $message, $duration);
     }
 
+    /**
+     * @param string $type
+     * @param string $message
+     * @param int $duration
+     */
     public static function createNotification(string $type, string $message, int $duration)
     {
 
@@ -24,24 +33,30 @@ class Notificator
         session()->flash('notifications.' . $notification->getId(), $notification);
     }
 
-    public static function getUniqueId(): string
-    {
-
-        return uniqid();
-    }
-
+    /**
+     * @param string $message
+     * @param int $duration
+     */
     public static function warning(string $message, int $duration = 5000)
     {
 
         self::createNotification('warning', $message, $duration);
     }
 
+    /**
+     * @param string $message
+     * @param int $duration
+     */
     public static function info(string $message, int $duration = 5000)
     {
 
         self::createNotification('info', $message, $duration);
     }
 
+    /**
+     * @param string $message
+     * @param int $duration
+     */
     public static function error(string $message, int $duration = 5000)
     {
 
